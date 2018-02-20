cbdbf67ffa6ad2ac59badf87197c1d3e677d48e7
Aaron <aaron.tkd@hotmail.com>
2018-02-20 15:35:32 +0000

    code cleanup

--- scripts/network_setup.sh
+++ scripts/network_setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#route add default netmask 255.255.255.0 gw 10.211.55.1
-
 iptables -t nat -A PREROUTING -d 10.211.54.200 -j DNAT --to-destination 10.211.55.200
-#iptables -t nat -A POSTROUTING -s 10.211.55.200 -j SNAT --to-source 10.211.54.200
 
 cp /vagrant/resources/hosts /etc/hosts
--- scripts/setup-java.sh
+++ scripts/setup-java.sh
@@ -1,21 +1,6 @@
     #!/bin/bash
 
-    #apt-get -y -q update
-    #apt-get -y -q upgrade
-
-    #apt-get -y -q install software-properties-common htop
-
-    #add-apt-repository -y ppa:webupd8team/java
-    #apt-get -y -q update
-
-    #echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-    #echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-    #apt-get -y -q install oracle-java8-installer
-    
-    #update-java-alternatives -s java-8-oracle
-
     add-apt-repository -y ppa:openjdk-r/ppa
 
-    apt-get -y -q update
-    
+    apt-get -y -q update   
     apt-get -y install openjdk-8-jdk
